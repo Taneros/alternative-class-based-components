b5ea57cc6f90da80a5f51b969b94c53cee7e9f6d
Taneros <fricentaurus@GMAIL.COM>
2022-08-28 12:19:50 +0300

    refactor: convert Users

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import {Component, useState } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
@@ -9,29 +9,58 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ];
 
-const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
 
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
-  };
+class Users extends Component {
+  
+  constructor() {
+    super()
+    this.state = {
+      showUsers: true,
+    }
+    this.toggleUsersHandler = this.toggleUsersHandler.bind(this)
+  }
 
-  const usersList = (
+  toggleUsersHandler() {
+    this.setState((prevState) => { return {showUsers: !prevState.showUsers}})
+  }
+  
+  render(){
+    
+      const usersList = (
     <ul>
       {DUMMY_USERS.map((user) => (
         <User key={user.id} name={user.name} />
       ))}
     </ul>
   );
-
-  return (
+    
+    return (
     <div className={classes.users}>
-      <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
+      <button onClick={this.toggleUsersHandler}>
+        {this.state.showUsers ? 'Hide' : 'Show'} Users
       </button>
-      {showUsers && usersList}
+      {this.state.showUsers && usersList}
     </div>
-  );
-};
+  );}
+}
+
+// const Users = () => {
+//   const [showUsers, setShowUsers] = useState(true);
+
+//   const toggleUsersHandler = () => {
+//     setShowUsers((curState) => !curState);
+//   };
+
+
+
+//   return (
+//     <div className={classes.users}>
+//       <button onClick={toggleUsersHandler}>
+//         {showUsers ? 'Hide' : 'Show'} Users
+//       </button>
+//       {showUsers && usersList}
+//     </div>
+//   );
+// };
 
 export default Users;
